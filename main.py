6d745fb86d569135f6b6f4cde4258c9f8a9449a2
deloygintaras <turcinasgintaras@gmail.com>
2024-10-17 21:13:54 +0300

    Files

---
+++ main.py
@@ -0,0 +1,52 @@
+#
+# failas = open('./file.txt')
+# nuskaitytas_tekstas = failas.read()
+# print(nuskaitytas_tekstas)
+# failas.close()
+#
+#
+# failas = open('./file.txt')
+# nuskaitytas_tekstas = failas.read()
+# print("Nuskaitytas:\n",nuskaitytas_tekstas)
+# print()
+#
+# kitas_tekstas = failas.read()
+# print("kitas:", kitas_tekstas)
+#
+# failas.close()
+
+# failas = open('./file.txt')
+# nuskaitytas_tekstas = failas.read()
+# print("Nuskaitytas:",nuskaitytas_tekstas)
+# print()
+#
+# failas.seek(0)
+# per_nauja = failas.read()
+# print("Naujas:", per_nauja)
+# failas.close()
+
+# failas = open('./file.txt')
+# print("Nuskaitytas tekstas", failas.read())
+# failas.close()
+
+# with open('./file.txt') as failas:
+#     tekstas = failas.read()
+#     print(tekstas)
+#     print()
+#     failas.seek(10)
+#     atnaujinimas = failas.read()
+#     print("Naujas: ", atnaujinimas)
+
+
+# with open('./file.txt') as failas:
+#     eilute = failas.readline()
+#     print(eilute)
+#     eilute = failas.readline()
+#     print(eilute)
+#     eilute = failas.readline()
+#     print(eilute)
+
+# with open('./file.txt') as failas:
+#     visas = failas.readlines()
+#     print(visas)
+
